92683dd02d727753f1509716d4e10c4e158d9fd4
Anshul Mistry <amm.suthar@gmail.com>
2020-08-19 20:30:39 -0400

    chart

--- src/api/index.js
+++ src/api/index.js
@@ -18,5 +18,10 @@ const url='https://covid19.mathdro.id/api';
 }
 
 export const dailydata = async () =>{
-    
+    try{
+        const {data} = await axios.get(`${url}/daily`);
+        return data
+    } catch (errors){
+
+    }
 }
--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
+import {dailydata} from '../../api/index';
+import {Line,Bar} from 'react-chartjs-2';
+import styles from './Chart.module.css';
 
-function Chart() {
+const Chart = () => {
+  const [daily,setdaily] = useState([]);
+
+  useEffect(()=>{
+    const getdata = async () =>{
+      setdaily(await dailydata()); 
+    }
+    getdata()
+
+  },[])
+  console.log(daily)
+
+  const lineChart = (
+    daily.length ? (
+    <Line
+      data={{
+        labels:daily.map(({ reportDate }) => reportDate),
+        datasets:[{
+          data: daily.map((data) => data.confirmed.total),
+          label:'Infected',
+          borderColor:'#3333ff',
+          fill:true
+        },{
+          data: daily.map((data) => data.deaths.total),
+          label:'Deaths',
+          borderColor:'red',
+          backgroundColor:'rgba(255,0,0,0.5)',
+          fill:true
+        }]
+      }}
+    />
+    ):null
+  );
+
   return (
     <div className="Chart">
-      <h1>Covid19 Tracker</h1>
+      <div className={styles.container}>
+        {lineChart}
+      </div>
     </div>
   );
 }
